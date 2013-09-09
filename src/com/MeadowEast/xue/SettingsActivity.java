a988f5ad5a181abfe37baa6e974a92d8878805ab
Elliott Hankins <hankinse@gmail.com>
2013-09-09 01:05:21 -0400

    The settings button on the main menu should now actually be listening for a click.

--- src/com/MeadowEast/xue/SettingsActivity.java
+++ src/com/MeadowEast/xue/SettingsActivity.java
@@ -1,14 +1,33 @@
 package com.MeadowEast.xue;
 
+import java.io.File;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class SettingsActivity extends Activity implements OnClickListener {
-
+	Button updateButton;
+	public static File filesDir;
+	static final String TAG = "XUE SettingsActivity";
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        updateButton   = (Button) findViewById(R.id.update_button);
+    	updateButton.setOnClickListener(this);
+        File sdCard = Environment.getExternalStorageDirectory();
+		filesDir = new File (sdCard.getAbsolutePath() + "/Android/data/com.MeadowEast.xue/files");
+		Log.d(TAG, "xxx filesDir="+filesDir);
+    }
+    
 	public void onClick(View arg0) {
-	   	Intent i;
 	   	switch (arg0.getId()){
 	   	case R.id.update_button:
 	   		
